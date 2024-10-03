7f191eb17248fb5383dbfd73f01bc20aef94d37e
Nbiba Bedis <bedisnbiba@gmail.com>
2024-10-03 21:05:01 +0100

    add server for web version

---
+++ web/server.ts
@@ -0,0 +1,9 @@
+import { serveFile } from "jsr:@std/http@1.0/file-server";
+
+Deno.serve((req) => {
+  const url = req.url;
+  const path = new URL(url).pathname;
+  if (path === "/") {
+    return serveFile(req, "web/index.html");
+  }
+});
